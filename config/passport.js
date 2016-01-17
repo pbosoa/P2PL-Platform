ed90688584153e6f3cc693d823961c164f1ccf76
Jay Kumar <jay.sivan.kumar@gmail.com>
2016-01-17 09:23:29 -0800

    Added facebook passport strategy

--- config/passport.js
+++ config/passport.js
@@ -69,3 +69,36 @@ module.exports = function(passport){
     });
   }
   ));
+
+  passport.use(new FacebookStrategy({
+    clientID: configAuth.facebookAuth.clientID,
+    clientSecret: configAuth.facebookAuth.clientSecret,
+    callbackURL: configAuth.facebookAuth.callbackURL,
+    profileFields: ["emails", "displayName", "name"]
+  },
+  function(token, refreshToken, profile, done){
+    process.nextTick(function(){
+      User.findOne({'facebook.id': profile.id}, function(err, user){
+        if (err){
+          return done(err);
+        }
+        if (user){
+          return done(null, user);
+        }
+        else {
+          var newUser = new User();
+          newUser.facebook.id = profile.id;
+          newUser.facebook.token = token;
+          newUser.facebook.name = profile.name.giveName + ' ' + profile.name.familyName;
+          newUser.facebook.email = profile.emails[0].value;
+
+          newUser.save(function(err){
+            if (err){
+              throw err;
+            }
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
