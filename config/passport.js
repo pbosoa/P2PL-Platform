c0aaa8ace8a1bb09219f0d197334adc61e595d04
Jay Kumar <jay.sivan.kumar@gmail.com>
2016-01-17 09:31:50 -0800

    Issue #1: Added facebook strategy and authentication routes

--- config/passport.js
+++ config/passport.js
@@ -102,3 +102,35 @@ module.exports = function(passport){
       });
     });
   }));
+
+  passport.use(new TwitterStrategy({
+    consumerKey: configAuth.twitterAuth.consumerKey,
+    consumerSecret: configAuth.twitterAuth.consumerSecret,
+    callbackURL: configAuth.twitterAuth.callbackURL
+  },
+  function(token, tokenSecret, profile, done){
+    process.nextTick(function(){
+      User.findOne({'twitter.id': profile.id}, function(err, user){
+        if (err){
+          return done(err);
+        }
+        if (user){
+          return done(null, user);
+        }
+        else {
+          var newUser = new User();
+          newUser.twitter.id = profile.id;
+          newUser.twitter.token = token;
+          newUser.twitter.username = profile.username;
+          newUser.twitter.displayName = profile.displayName;
+
+          newUser.save(function(err){
+            if (err){
+              throw err;
+            }
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
