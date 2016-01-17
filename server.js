15b9a3be61d076e43aa856f5a61a20dd5321bc40
Jay Kumar <jay.sivan.kumar@gmail.com>
2016-01-17 08:46:17 -0800

    Set-up initial node server file and imported required modules

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require ('connect-flash');
+
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+require('./config/passport')(passport);
+
+// set-up for express application
+app.use(express.static('/'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//required for passport
+app.use(session({secret: 'thesecret', resave: false, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//routes
+require('./app/routes.js')(app, passport);
+
+//launch
+app.listen(1337);
+console.log('Magic on port 1337');
